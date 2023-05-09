6dfc919998b5aa70d3d21f1d6c122ca604dcd75c
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-05-09 23:16:35 +0400

    feat: add properties and button to draw graphs

--- AeroDynamicCalculator/Form1.Designer.cs
+++ AeroDynamicCalculator/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.labelModel = new System.Windows.Forms.Label();
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.labelGeometry = new System.Windows.Forms.Label();
@@ -45,17 +48,21 @@
             this.labelalpha = new System.Windows.Forms.Label();
             this.trackBar = new System.Windows.Forms.TrackBar();
             this.alphaValue = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonGraph = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelModel
             // 
             this.labelModel.AutoSize = true;
             this.labelModel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelModel.Location = new System.Drawing.Point(29, 27);
             this.labelModel.Name = "labelModel";
-            this.labelModel.Size = new System.Drawing.Size(57, 16);
+            this.labelModel.Size = new System.Drawing.Size(84, 25);
             this.labelModel.TabIndex = 0;
             this.labelModel.Text = "Модель";
             // 
@@ -73,9 +80,10 @@
             // 
             this.labelGeometry.AutoSize = true;
             this.labelGeometry.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelGeometry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelGeometry.Location = new System.Drawing.Point(433, 27);
             this.labelGeometry.Name = "labelGeometry";
-            this.labelGeometry.Size = new System.Drawing.Size(224, 16);
+            this.labelGeometry.Size = new System.Drawing.Size(327, 25);
             this.labelGeometry.TabIndex = 2;
             this.labelGeometry.Text = "Геометрические характеристики";
             // 
@@ -83,84 +91,99 @@
             // 
             this.labelR.AutoSize = true;
             this.labelR.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelR.Location = new System.Drawing.Point(433, 90);
             this.labelR.Name = "labelR";
-            this.labelR.Size = new System.Drawing.Size(30, 16);
+            this.labelR.Size = new System.Drawing.Size(47, 25);
             this.labelR.TabIndex = 3;
             this.labelR.Text = "R = ";
             // 
             // textBoxR
             // 
+            this.textBoxR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxR.Location = new System.Drawing.Point(487, 87);
             this.textBoxR.Name = "textBoxR";
-            this.textBoxR.Size = new System.Drawing.Size(241, 22);
+            this.textBoxR.Size = new System.Drawing.Size(241, 30);
             this.textBoxR.TabIndex = 4;
+            this.textBoxR.Text = "0,4";
             // 
             // textBoxRn
             // 
+            this.textBoxRn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxRn.Location = new System.Drawing.Point(487, 115);
             this.textBoxRn.Name = "textBoxRn";
-            this.textBoxRn.Size = new System.Drawing.Size(241, 22);
+            this.textBoxRn.Size = new System.Drawing.Size(241, 30);
             this.textBoxRn.TabIndex = 6;
+            this.textBoxRn.Text = "0,3";
             // 
             // labelRn
             // 
             this.labelRn.AutoSize = true;
             this.labelRn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelRn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelRn.Location = new System.Drawing.Point(433, 118);
             this.labelRn.Name = "labelRn";
-            this.labelRn.Size = new System.Drawing.Size(31, 16);
+            this.labelRn.Size = new System.Drawing.Size(51, 25);
             this.labelRn.TabIndex = 5;
             this.labelRn.Text = "rn = ";
             // 
             // textBoxTetha
             // 
+            this.textBoxTetha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxTetha.Location = new System.Drawing.Point(487, 143);
             this.textBoxTetha.Name = "textBoxTetha";
-            this.textBoxTetha.Size = new System.Drawing.Size(241, 22);
+            this.textBoxTetha.Size = new System.Drawing.Size(241, 30);
             this.textBoxTetha.TabIndex = 8;
+            this.textBoxTetha.Text = "45";
             // 
             // labelTetha
             // 
             this.labelTetha.AutoSize = true;
             this.labelTetha.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelTetha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelTetha.Location = new System.Drawing.Point(433, 146);
             this.labelTetha.Name = "labelTetha";
-            this.labelTetha.Size = new System.Drawing.Size(27, 16);
+            this.labelTetha.Size = new System.Drawing.Size(45, 25);
             this.labelTetha.TabIndex = 7;
             this.labelTetha.Text = "θ = ";
             // 
             // textBoxLc
             // 
+            this.textBoxLc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxLc.Location = new System.Drawing.Point(487, 171);
             this.textBoxLc.Name = "textBoxLc";
-            this.textBoxLc.Size = new System.Drawing.Size(241, 22);
+            this.textBoxLc.Size = new System.Drawing.Size(241, 30);
             this.textBoxLc.TabIndex = 10;
+            this.textBoxLc.Text = "0,3";
             // 
             // labelLc
             // 
             this.labelLc.AutoSize = true;
             this.labelLc.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelLc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelLc.Location = new System.Drawing.Point(433, 174);
             this.labelLc.Name = "labelLc";
-            this.labelLc.Size = new System.Drawing.Size(34, 16);
+            this.labelLc.Size = new System.Drawing.Size(55, 25);
             this.labelLc.TabIndex = 9;
             this.labelLc.Text = "Lc = ";
             // 
             // textBoxL
             // 
+            this.textBoxL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBoxL.Location = new System.Drawing.Point(487, 199);
             this.textBoxL.Name = "textBoxL";
-            this.textBoxL.Size = new System.Drawing.Size(241, 22);
+            this.textBoxL.Size = new System.Drawing.Size(241, 30);
             this.textBoxL.TabIndex = 12;
+            this.textBoxL.Text = "0,35";
             // 
             // labelL
             // 
             this.labelL.AutoSize = true;
             this.labelL.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelL.Location = new System.Drawing.Point(433, 202);
             this.labelL.Name = "labelL";
-            this.labelL.Size = new System.Drawing.Size(27, 16);
+            this.labelL.Size = new System.Drawing.Size(45, 25);
             this.labelL.TabIndex = 11;
             this.labelL.Text = "L = ";
             // 
@@ -168,9 +191,10 @@
             // 
             this.labelalpha.AutoSize = true;
             this.labelalpha.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.labelalpha.Location = new System.Drawing.Point(433, 253);
+            this.labelalpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelalpha.Location = new System.Drawing.Point(406, 253);
             this.labelalpha.Name = "labelalpha";
-            this.labelalpha.Size = new System.Drawing.Size(54, 16);
+            this.labelalpha.Size = new System.Drawing.Size(82, 25);
             this.labelalpha.TabIndex = 13;
             this.labelalpha.Text = "alpha = ";
             // 
@@ -188,17 +212,47 @@
             // 
             this.alphaValue.AutoSize = true;
             this.alphaValue.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.alphaValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.alphaValue.Location = new System.Drawing.Point(714, 253);
             this.alphaValue.Name = "alphaValue";
-            this.alphaValue.Size = new System.Drawing.Size(18, 16);
+            this.alphaValue.Size = new System.Drawing.Size(31, 25);
             this.alphaValue.TabIndex = 15;
             this.alphaValue.Text = "0°";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(32, 415);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(766, 300);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            title1.Name = "TitleCy";
+            title1.Text = "Зависимость Cy от угла атаки alpha";
+            this.chart1.Titles.Add(title1);
+            // 
+            // buttonGraph
+            // 
+            this.buttonGraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGraph.Location = new System.Drawing.Point(438, 295);
+            this.buttonGraph.Name = "buttonGraph";
+            this.buttonGraph.Size = new System.Drawing.Size(322, 60);
+            this.buttonGraph.TabIndex = 17;
+            this.buttonGraph.Text = "Построить графики";
+            this.buttonGraph.UseVisualStyleBackColor = true;
+            this.buttonGraph.Click += new System.EventHandler(this.buttonGraph_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(926, 564);
+            this.ClientSize = new System.Drawing.Size(822, 758);
+            this.Controls.Add(this.buttonGraph);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.alphaValue);
             this.Controls.Add(this.trackBar);
             this.Controls.Add(this.labelalpha);
@@ -216,10 +270,12 @@
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.labelModel);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Расчет аэродинамических характеристик летательного аппарата";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,6 +299,8 @@
         private System.Windows.Forms.Label labelalpha;
         private System.Windows.Forms.TrackBar trackBar;
         private System.Windows.Forms.Label alphaValue;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonGraph;
     }
 }
 
--- AeroDynamicCalculator/Form1.cs
+++ AeroDynamicCalculator/Form1.cs
@@ -12,6 +12,76 @@ namespace AeroDynamicCalculator
 {
     public partial class Form1 : Form
     {
+        double r, rn, tetha, lc, l;
+        int alpha;
+
+        public double R
+        {
+            get
+            {
+                return r;
+            }
+            set
+            {
+                r = value;
+            }
+        }
+        public double Rn
+        {
+            get
+            {
+                return rn;
+            }
+            set
+            {
+                rn = value;
+            }
+        }
+        public double Tetha
+        {
+            get
+            {
+                return tetha;
+            }
+            set
+            {
+                tetha = value;
+            }
+        }
+        public double Lc
+        {
+            get
+            {
+                return lc;
+            }
+            set
+            {
+                lc = value;
+            }
+        }
+        public double L
+        {
+            get
+            {
+                return l;
+            }
+            set
+            {
+                l = value;
+            }
+        }
+        public int Alpha
+        {
+            get
+            {
+                return alpha;
+            }
+            set
+            {
+                alpha = value;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +95,16 @@ namespace AeroDynamicCalculator
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             alphaValue.Text = trackBar.Value.ToString() + "°";
+        }
+
+        private void buttonGraph_Click(object sender, EventArgs e)
+        {
+            R = Convert.ToDouble(textBoxR.Text);
+            Rn = Convert.ToDouble(textBoxRn.Text);
+            Tetha = Convert.ToDouble(textBoxTetha.Text);
+            Lc = Convert.ToDouble(textBoxLc.Text);
+            L = Convert.ToDouble(textBoxL.Text);
+            Alpha = trackBar.Value;
         }
     }
 }
