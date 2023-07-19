48e27d983e027dd2ec99ed6d1dd10f26fa1c893f
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-07-19 14:18:47 +0400

    creating statis class with formulas

--- AeroDynamicCalculator/Form1.cs
+++ AeroDynamicCalculator/Form1.cs
@@ -14,56 +14,57 @@ namespace AeroDynamicCalculator
 {
     public partial class Form1 : Form
     {
-        const double PI = Math.PI;
-        double r, rn, tetha, alpha;
-        double cyn, cx, mzn;
+        Formulas capsule;
+        //const double PI = Math.PI;
+        //double r, rn, tetha, alpha;
+        //double cyn, cx, mzn;
 
 
-        public double R
-        {
-            get
-            {
-                return r;
-            }
-            set
-            {
-                r = value;
-            }
-        }
-        public double Rn
-        {
-            get
-            {
-                return rn;
-            }
-            set
-            {
-                rn = value;
-            }
-        }
-        public double Tetha
-        {
-            get
-            {
-                return tetha;
-            }
-            set
-            {
-                tetha = value;
-            }
-        }
+        //public double R
+        //{
+        //    get
+        //    {
+        //        return r;
+        //    }
+        //    set
+        //    {
+        //        r = value;
+        //    }
+        //}
+        //public double Rn
+        //{
+        //    get
+        //    {
+        //        return rn;
+        //    }
+        //    set
+        //    {
+        //        rn = value;
+        //    }
+        //}
+        //public double Tetha
+        //{
+        //    get
+        //    {
+        //        return tetha;
+        //    }
+        //    set
+        //    {
+        //        tetha = value;
+        //    }
+        //}
 
-        public double Alpha
-        {
-            get
-            {
-                return alpha;
-            }
-            set
-            {
-                alpha = value;
-            }
-        }
+        //public double Alpha
+        //{
+        //    get
+        //    {
+        //        return alpha;
+        //    }
+        //    set
+        //    {
+        //        alpha = value;
+        //    }
+        //}
 
 
         public Form1()
@@ -73,6 +74,7 @@ namespace AeroDynamicCalculator
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             // configuration for Charts
             // example: chartName.ChartAreas[0].AxisY.Maximum = 100;
 
@@ -81,7 +83,7 @@ namespace AeroDynamicCalculator
             chartCx.ChartAreas[0].AxisX.Title = "alpha";
             chartCx.ChartAreas[0].AxisY.Title = "Cx";
             chartCx.Series[0].XValueType = ChartValueType.Double;
-            
+
 
             // chart Cy
             chartCy.ChartAreas[0].AxisX.LabelStyle.Format = "n3";
@@ -119,13 +121,10 @@ namespace AeroDynamicCalculator
             }
             else
             {
-
-
-                R = Convert.ToDouble(textBoxR.Text);
+                capsule = new Formulas(Convert.ToDouble(textBoxR.Text), Convert.ToDouble(textBoxRn.Text), Convert.ToDouble(textBoxTetha.Text) * Math.PI / 180);
+                /*R = Convert.ToDouble(textBoxR.Text);
                 Rn = Convert.ToDouble(textBoxRn.Text);
-                Tetha = Convert.ToDouble(textBoxTetha.Text) * PI / 180;
-
-
+                Tetha = Convert.ToDouble(textBoxTetha.Text) * PI / 180;*/
 
                 chartCx.Series[0].Points.Clear();
                 chartCy.Series[0].Points.Clear();
@@ -180,14 +179,14 @@ namespace AeroDynamicCalculator
             //
             // counting aerodynamic params for a spherecial segment
             //
-            double cynS, cxS, beta, gamma, a;
+            //double cynS, cxS, beta, gamma, a;
             double eps = 0.0000000000001;
 
             //Pass the filepath and filename to the StreamWriter Constructor
             //StreamWriter sw = new StreamWriter("C:\\Users\\Denis\\Desktop\\directory\\Test.txt");
-            
 
-            for (double i = 0; i <= 180; i++)
+
+            /*for (double i = 0; i <= 180; i++)
             {
                 Alpha = i * PI / 180;
 
@@ -215,81 +214,84 @@ namespace AeroDynamicCalculator
                        * Math.Pow(Math.Cos(Tetha), 2) + gamma * Math.Cos(Alpha) / PI
                        + (Math.Cos(Alpha) / (2 * PI)) * Math.Sin(Tetha)
                        * (1 - 3 * Math.Pow(Math.Sin(Tetha), 2)) * a;
-                }
+                }*/
 
+            for (double i = 0; i <= 180; i++)
+            {
+                capsule.calculateValues(i, eps);
 
                 //
                 // counting aerodynamic params for a truncated cone
                 //
 
-                double cynC, cxC;
+                //double cynC, cxC;
 
-                // 0 <= Alpha <= Tetha
-                if (Alpha >= eps && Alpha <= Tetha)
-                {
-                    cynC = 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha);
-                    cxC = 2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
-                        * Math.Pow(Math.Sin(Alpha), 2);
-                }
-                // Tetha < Alpha <= PI/2
-                else
-                {
-                    double tangens = Math.Tan(Tetha) / Math.Tan(Alpha);
-                    cynC = 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha)
-                        * (1 + 2 * beta / PI + (2 / (3 * PI))
-                        * Math.Sqrt(1 - Math.Pow(tangens, 2)) * (2 * tangens + Math.Pow(tangens, -1)));
-                    cxC = 0.5 * (1 + 2 * beta / PI)
-                        * (2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
-                        * Math.Pow(Math.Sin(Alpha), 2)) + (0.75 / PI) * Math.Sqrt(1 - Math.Pow(tangens, 2))
-                        * Math.Sin(2 * Alpha) * Math.Sin(2 * Tetha);
-                }
+                //// 0 <= Alpha <= Tetha
+                //if (Alpha >= eps && Alpha <= Tetha)
+                //{
+                //    cynC = 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha);
+                //    cxC = 2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
+                //        * Math.Pow(Math.Sin(Alpha), 2);
+                //}
+                //// Tetha < Alpha <= PI/2
+                //else
+                //{
+                //    double tangens = Math.Tan(Tetha) / Math.Tan(Alpha);
+                //    cynC = 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha)
+                //        * (1 + 2 * beta / PI + (2 / (3 * PI))
+                //        * Math.Sqrt(1 - Math.Pow(tangens, 2)) * (2 * tangens + Math.Pow(tangens, -1)));
+                //    cxC = 0.5 * (1 + 2 * beta / PI)
+                //        * (2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
+                //        * Math.Pow(Math.Sin(Alpha), 2)) + (0.75 / PI) * Math.Sqrt(1 - Math.Pow(tangens, 2))
+                //        * Math.Sin(2 * Alpha) * Math.Sin(2 * Tetha);
+                //}
 
                 //
                 // counting cone with sperical segnent
                 //
 
-                double rn = Rn / R;
-                cyn = cynS * Math.Pow(rn, 2) + cynC * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
-                cx = -cxS * Math.Pow(rn, 2) - cxC * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
+                //double rn = Rn / R;
+                //cyn = cynS * Math.Pow(rn, 2) + cynC * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
+                //cx = -cxS * Math.Pow(rn, 2) - cxC * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
 
 
-                // capsule length
-                double L = R / Math.Tan(Tetha) + Rn - Rn / Math.Sin(Tetha);
-                // truncated cone length
-                double Lc = (R - Rn * Math.Cos(Tetha)) * (1.0 / Math.Tan(Tetha));
+                //// capsule length
+                //double L = R / Math.Tan(Tetha) + Rn - Rn / Math.Sin(Tetha);
+                //// truncated cone length
+                //double Lc = (R - Rn * Math.Cos(Tetha)) * (1.0 / Math.Tan(Tetha));
 
-                double L0 = L / R * Math.Tan(Tetha);
+                //double L0 = L / R * Math.Tan(Tetha);
 
-                double RL = Rn / R * Math.Cos(Tetha);
-                // value interpretates the center of truncated cone
-                double xd = 2 / (3 * Math.Pow(Math.Cos(Tetha), 2)) *
-                    ((1 + RL + RL * RL) / (1 - RL * RL)) - RL / (1 - RL);
+                //double RL = Rn / R * Math.Cos(Tetha);
+                //// value interpretates the center of truncated cone
+                //double xd = 2 / (3 * Math.Pow(Math.Cos(Tetha), 2)) *
+                //    ((1 + RL + RL * RL) / (1 - RL * RL)) - RL / (1 - RL);
 
-                mzn = -cynS * Math.Tan(Tetha) / L0 * Math.Pow(rn, 3)
-                    - cynC / L0 * (1 - Math.Pow(rn * Math.Cos(Tetha), 2))
-                    * (xd * Lc / L + (1 - Lc / L));
+                //mzn = -cynS * Math.Tan(Tetha) / L0 * Math.Pow(rn, 3)
+                //    - cynC / L0 * (1 - Math.Pow(rn * Math.Cos(Tetha), 2))
+                //    * (xd * Lc / L + (1 - Lc / L));
 
                 // !!!!! I have question!!!! 
-                double xC = -1;
-                //
+                //double xC = -1;
+                ////
 
-                double mzc = mzn - cyn * xC;
-                double xD = mzn / cyn;
+                //double mzc = mzn - cyn * xC;
+                //double xD = mzn / cyn;
 
 
-                chartCx.Series[0].Points.AddXY(i, cx);
-                chartCy.Series[0].Points.AddXY(i, cyn);
-                chartM.Series[0].Points.AddXY(i, mzn);
-                chartMzc.Series[0].Points.AddXY(i, mzc);
-                chartXD.Series[0].Points.AddXY(i, xD);
+                chartCx.Series[0].Points.AddXY(i, capsule.Cx);
+                chartCy.Series[0].Points.AddXY(i, capsule.Cyn);
+                chartM.Series[0].Points.AddXY(i, capsule.Mzn);
+                chartMzc.Series[0].Points.AddXY(i, capsule.Mzc);
+                chartXD.Series[0].Points.AddXY(i, capsule.XD);
 
                 //sw.WriteLine($"{i}  {cx}");
             }
-
-            //Close the file
-            //sw.Close();
         }
 
-        
+        //Close the file
+        //sw.Close();
     }
+
+
 }
---
+++ AeroDynamicCalculator/Formulas.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeroDynamicCalculator
+{
+    class Formulas
+    {
+        // constants
+        const double PI = Math.PI;
+
+        // input values
+        double r, rn, tetha, alpha;
+        double cyn, cx, mzn, mzc, xD;
+
+        public double R
+        {
+            get
+            {
+                return r;
+            }
+        }
+        public double Rn
+        {
+            get
+            {
+                return rn;
+            }
+        }
+        public double Tetha
+        {
+            get
+            {
+                return tetha;
+            }
+        }
+
+        public double Alpha
+        {
+            get
+            {
+                return alpha;
+            }
+            set
+            {
+                alpha = value;
+            }
+        }
+
+        public double Cyn
+        {
+            get
+            {
+                return cyn;
+            }
+        }
+        public double Cx
+        {
+            get
+            {
+                return cx;
+            }
+        }
+        public double Mzn
+        {
+            get
+            {
+                return mzn;
+            }
+        }
+        public double Mzc
+        {
+            get
+            {
+                return mzc;
+            }
+        }
+        public double XD
+        {
+            get
+            {
+                return xD;
+            }
+        }
+
+        public Formulas()
+        {
+            r = 0.4;
+            rn = 0.3;
+            tetha = 45;
+        }
+
+        public Formulas(double r, double rn, double tetha)
+        {
+            this.r = r; 
+            this.rn = rn; 
+            this.tetha = tetha; 
+        }
+
+        internal void calculateValues(double iteration, double eps)
+        {
+            Alpha = iteration * PI / 180;
+            cyn = CountCyn(eps);
+            cx = CountCx(eps);
+            mzn = CountMzn(eps);
+            mzc = CountMzc(eps);
+            xD = CountXD(eps);
+        }
+
+        internal double CountBeta()
+        {
+            return Math.Asin(Math.Tan(Tetha) / Math.Tan(Alpha));
+        }
+
+        internal double CountGamma()
+        {
+            return Math.Acos(Math.Sin(Tetha) / Math.Sin(Alpha));
+        }
+
+        internal double CountA()
+        {
+            return Math.Sqrt(Math.Pow(Math.Sin(Alpha), 2) - Math.Pow(Math.Sin(Tetha), 2));
+        }
+
+        //
+        // counting aerodynamic params for a truncated cone
+        //
+
+        internal double CountCynS(double eps)
+        {
+            // 0 <= Alpha <= Tetha
+            if (Alpha >= eps && Alpha <= Tetha)
+            {
+                return 0.5 * Math.Pow(Math.Cos(Tetha), 4) * Math.Sin(2 * Alpha);
+            }
+            // Tetha < Alpha <= PI/2
+            else
+            {
+                return 0.25 * Math.Pow(Math.Cos(Tetha), 4) * Math.Sin(2 * Alpha) * (1 + 2 * CountBeta() / PI)
+                        + CountGamma() * Math.Sin(Alpha) / PI + (1 / (3 * PI)) * Math.Sin(Alpha) * Math.Sin(Tetha)
+                        * (Math.Pow(Math.Sin(Tetha), 2) * (3 - Math.Pow(Math.Sin(Alpha), -2))) * CountA();
+            }
+        }
+
+        internal double CountCxS(double eps)
+        {
+            // 0 <= Alpha <= Tetha
+            if (Alpha >= eps && Alpha <= Tetha)
+            {
+                return 2 * Math.Pow(Math.Cos(Tetha), 2) * (1 - 0.5 * Math.Pow(Math.Cos(Tetha), 2)
+                       - (1 - 0.75 * Math.Pow(Math.Cos(Tetha), 2)) * Math.Pow(Math.Sin(Alpha), 2));
+            }
+            // Tetha < Alpha <= PI/2
+            else
+            {
+                return (1 + 2 * CountBeta() / PI) * (1 - 0.5 * Math.Pow(Math.Cos(Tetha), 2)
+                       - (1 - 0.75 * Math.Pow(Math.Cos(Tetha), 2)) * Math.Pow(Math.Sin(Alpha), 2))
+                       * Math.Pow(Math.Cos(Tetha), 2) + CountGamma() * Math.Cos(Alpha) / PI
+                       + (Math.Cos(Alpha) / (2 * PI)) * Math.Sin(Tetha)
+                       * (1 - 3 * Math.Pow(Math.Sin(Tetha), 2)) * CountA();
+            }
+        }
+
+        internal double CountCynC(double eps)
+        {
+            // 0 <= Alpha <= Tetha
+            if (Alpha >= eps && Alpha <= Tetha)
+            {
+                return 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha);
+            }
+            // Tetha < Alpha <= PI/2
+            else
+            {
+                double tangens = Math.Tan(Tetha) / Math.Tan(Alpha);
+                return 0.5 * Math.Pow(Math.Cos(Tetha), 2) * Math.Sin(2 * Alpha)
+                        * (1 + 2 * CountBeta() / PI + (2 / (3 * PI)) * Math.Sqrt(1 - Math.Pow(tangens, 2)) 
+                        * (2 * tangens + Math.Pow(tangens, -1)));
+            }
+        }
+
+        internal double CountCxC(double eps)
+        {
+            // 0 <= Alpha <= Tetha
+            if (Alpha >= eps && Alpha <= Tetha)
+            {
+                return 2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
+                        * Math.Pow(Math.Sin(Alpha), 2);
+            }
+            // Tetha < Alpha <= PI/2
+            else
+            {
+                double tangens = Math.Tan(Tetha) / Math.Tan(Alpha);
+                return 0.5 * (1 + 2 * CountBeta() / PI)
+                        * (2 * Math.Pow(Math.Sin(Tetha), 2) + (1 - 3 * Math.Pow(Math.Sin(Tetha), 2))
+                        * Math.Pow(Math.Sin(Alpha), 2)) + (0.75 / PI) * Math.Sqrt(1 - Math.Pow(tangens, 2))
+                        * Math.Sin(2 * Alpha) * Math.Sin(2 * Tetha);
+            }
+        }
+
+        //
+        // counting cone with sperical segnent
+        //
+
+        internal double CountRn()
+        {
+            return Rn / R;
+        }
+
+        internal double CountCyn(double eps)
+        {
+            return CountCynS(eps) * Math.Pow(rn, 2) + CountCynC(eps) * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
+        }
+
+        internal double CountCx(double eps)
+        {
+            return -CountCxS(eps) * Math.Pow(rn, 2) - CountCxC(eps) * (1 - Math.Pow(rn * Math.Cos(Tetha), 2));
+        }
+
+
+
+        // capsule length
+        internal double CountL()
+        {
+            return R / Math.Tan(Tetha) + Rn - Rn / Math.Sin(Tetha);
+        }
+
+        // truncated cone length
+        internal double CountLc()
+        {
+            return (R - Rn * Math.Cos(Tetha)) * (1.0 / Math.Tan(Tetha));
+        }
+
+        internal double CountL0()
+        {
+            return CountL() / R * Math.Tan(Tetha);
+        }
+
+        internal double CountRL()
+        {
+            return Rn / R * Math.Cos(Tetha);
+        }
+
+        // value interpretates the center of truncated cone
+        internal double CountXd()
+        {
+            double RL = CountRL();
+            return 2 / (3 * Math.Pow(Math.Cos(Tetha), 2)) *
+            ((1 + RL + RL * RL) / (1 - RL * RL)) - RL / (1 - RL);
+        }
+
+        internal double CountMzn(double eps)
+        {
+            return -CountCynS(eps) * Math.Tan(Tetha) / CountL0() * Math.Pow(rn, 3)
+                    - CountCynC(eps) / CountL0() * (1 - Math.Pow(rn * Math.Cos(Tetha), 2))
+                    * (CountXd() * CountLc() / CountL() + (1 - CountLc() / CountL()));
+        }
+
+        // !!!!! I have question!!!! 
+        double xC = -1;
+        //
+
+        internal double CountMzc(double eps)
+        {
+            return CountMzn(eps) - CountCyn(eps) * xC;
+        }
+
+        internal double CountXD(double eps)
+        {
+            return CountMzn(eps) / CountCyn(eps);
+        }
+
+    }
+}
